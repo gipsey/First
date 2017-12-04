d83702377ca3ecc2b4147cce5059bc41de5e233d
David Debre <david.debre@fortech.ro>
2017-12-04 13:24:13 +0200

    Initial Commit

---
+++ First/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First
+//
+//  Created by David Debre on 12/4/17.
+//  Copyright © 2017 David Debre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
