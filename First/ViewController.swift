c58558ca8daa5cadd6660f39255731ec6ccd27f6
David Debre <david.debre@fortech.ro>
2017-12-06 12:08:21 +0200

    asd

--- First/ViewController.swift
+++ First/ViewController.swift
@@ -8,16 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     // MARK: Properties
     @IBOutlet weak var asdasd: UILabel!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var goButton: UIButton!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     // MARK: Actions
     @IBAction func goButtonAction(_ sender: UIButton) {
         asdasd.text = "szia"
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    @IBAction func selectImageAction(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        
+        present(imagePickerController, animated: true, completion: nil)
     }
     
     // MARK: UITextFieldDelegate
@@ -28,6 +40,28 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     func textFieldDidEndEditing(_ textField: UITextField) {
         asdasd.text = textField.text
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            let alertView = UIAlertController(title: "image cannot be picked", message: ":(", preferredStyle: .alert)
+            present(alertView, animated: true)
+            return
+        }
+        
+        photoImageView.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        NSLog("imagePickerControllerDidCancel")
+        dismiss(animated: true, completion: nil)
+        
+        let alertView = UIAlertController(title: "why did u cancel? ", message: ":(", preferredStyle: .alert)
+        let action = UIAlertAction(title: "cancel", style: .cancel, handler: {(action: UIAlertAction!) in})
+        alertView.addAction(action)
+        present(alertView, animated: true)
     }
     
     override func viewDidLoad() {
