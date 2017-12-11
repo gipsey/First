95964c07708bb4de1c721560a4ddd9bc85da2fb0
David Debre <david.debre@fortech.ro>
2017-12-11 11:02:22 +0200

    fa

---
+++ First/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  First
+//
+//  Created by David Debre on 12/11/17.
+//  Copyright © 2017 David Debre. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
